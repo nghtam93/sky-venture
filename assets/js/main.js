2d4dd7053d36f0f9ecc919d209a8622b98c4ab32
doannguyen263 <doannguyen263@gmail.com>
2021-11-29 09:38:19 +0700

    Up

--- assets/js/main.js
+++ assets/js/main.js
@@ -140,12 +140,7 @@ $(document).ready(function(){
     }
 
     if($('body').hasClass( "single" )){
-        var single_share = $('.single__share')
-        var share_offset = $('.single__share').offset().top
 
-        $(window).scroll(function(){
-            $(this).scrollTop()>share_offset?single_share.addClass("is-active"):single_share.removeClass("is-active")
-        })
     }
 
 });
